6ce925e6e07030656c21d13436098b89568e68a5
Timofei Maksimov <maxtimofy@gmail.com>
2025-09-24 23:49:20 +0200

    feat: mix phx.gen.auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :sesh_logger, :scopes,
+  user: [
+    default: true,
+    module: SeshLogger.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: SeshLogger.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :sesh_logger,
   ecto_repos: [SeshLogger.Repo],
   generators: [timestamp_type: :utc_datetime]
